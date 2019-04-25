98e6b4305b3555be15c8c35fead4f8b4bc467e70
firaszakri <firaszakri1@gmail.com>
2019-04-25 22:31:23 +0200

    Add a String message

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
 	Date date= new Date();
         System.out.println(date);
+        String name= "Ahmad";
+        System.out.println(name);
     }
 }
